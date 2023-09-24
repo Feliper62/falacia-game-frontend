c3106d2a37ad0750c901f629037419002d91cf74
Feliper62 <felipe.oliveira6737@gmail.com>
2023-09-23 22:25:30 -0300

    :::feat: Adicionado e configurado componente Button

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import styles from './Button.module.css';
+import styles from '../styles/Button.module.css'; // Importe os estilos do CSS aqui
 
-const Botao = ({ text }) => {
-    return (
-      <button className={styles.button}>
-        {text}
-      </button>
-    );
-  }
+// O componente Button aceita props como text, href e id
+const Button = ({ text, href, id }) => {
+  return (
+    <a className={styles.Button} href={href} id={id}>
+      {text}
+    </a>
+  );
+};
+
+export default Button;
