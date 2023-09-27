4fd996f95c436161340a811b9dacc3ad6548fc9a
Feliper62 <felipe.oliveira6737@gmail.com>
2023-09-27 17:42:30 -0300

    :refactor: Adicionado responsividade entre botões

--- src/app/components/CaixaTexto.js
+++ src/app/components/CaixaTexto.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import styles from '../styles/CaixaTexto.module.css'; // Importe os estilos do CSS aqui
 import styles2 from '../styles/Texto.module.css'
-// const CaixaTexto = ({ label, placeholder }) => {
-//   return (
-//     <div className={styles.CaixaTexto}>
-//       <label className={styles.Label}>Apelido:</label>
-//       <div className={styles.InputContainer}>
-//         <input
-//           className={styles.Input}
-//           type="text"
-//           placeholder="Digite seu apelido aqui!"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CaixaTexto;
 
 const Input = ({text, placeholder}) => {
   return (
