6b0227f3ea58cf70d84f68772ca23f211e86f1ad
Veronica <marketing@lestscode.com.br>
2023-11-24 01:45:54 -0300

    :fix:Adicionado estilo na página grupo Jogador

--- src/app/pages/(inGame)/grupo/page.js
+++ src/app/pages/(inGame)/grupo/page.js
@@ -3,16 +3,27 @@ import styles from '../../../styles/Texto.module.css'
 import stylesText from '../../../styles/CaixaDeTexto.module.css'
 import CaixaDeTexto from "@/app/components/CaixaDeTexto";
 
+
 export default function GrupoConvidado(){
     return (
     <>
       <Texto className={styles.titulo} text='falácia'></Texto>
+      <div className={stylesText.container}>
+      <div> 
       <CaixaDeTexto className={stylesText.caixaDeTextoEsquerda} text='Aguardando Jogadores...'/>
       <CaixaDeTexto className={stylesText.caixaDeTextoEsquerda} text='Aguardando Inicio da Partida pelo Host...'/>
-      
+      </div>
+      <div>
+      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Jogador 1'/>
+      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Aguardando Jogadores'/>
+      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Aguardando Jogadores'/>
+      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Aguardando Jogadores'/>
+      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Aguardando Jogadores'/>
+      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Aguardando Jogadores'/>
+      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Aguardando Jogadores'/>
+      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Aguardando Jogadores'/>
+      </div>
+      </div>
     </>
     )
-  }
-  GrupoConvidado.getLayout = function getLayout(page) {
-    return <inGameLayout>{page}</inGameLayout>; // Substitua 'RootLayout' pelo seu layout personalizado, se necessário
-  };
+};
--- src/app/pages/(inGame)/layout.js
+++ src/app/pages/(inGame)/layout.js
@@ -8,19 +8,8 @@ import CaixaDeTexto from '@/app/components/CaixaDeTexto'
 export default function inGameLayout({ children }) {
     return (
       <>
-       
        {children}
-      
       <Button className={buttonStyle.voltarButton}  text="Voltar" href="/" id="voltar" />     
-      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Jogador 1'/>
-      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Espaço Vazio'/>
-      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Espaço Vazio'/>
-      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Espaço Vazio'/>
-      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Espaço Vazio'/>
-      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Espaço Vazio'/>
-      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Espaço Vazio'/>
-      <CaixaDeTexto className={stylesText.caixaDeTextoDireita} text='Espaço Vazio'/>
-  
       </>
         
     )
