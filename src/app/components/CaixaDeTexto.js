b57fb9ccea79176e25cc3d4505a98e8f9638b4aa
mpalmeida96 <mpalmeida96@hotmail.com>
2023-11-25 17:58:27 -0300

    ::test: teste de funcionalidades do backend com o frontedn

--- src/app/components/CaixaDeTexto.js
+++ src/app/components/CaixaDeTexto.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
 
-export default function CaixaDeTexto({ className, text }) {
-    return <div className={className} dangerouslySetInnerHTML={{ __html: text }}/>
+export default function CaixaDeTexto({ className, text, style }) {
+    return <div className={className} style={style}>{text}</div>
   }
--- src/app/pages/(menuPages)/teste/page.js
+++ src/app/pages/(menuPages)/teste/page.js
@@ -12,21 +12,49 @@ import api_client from '../../../api/route'
 
 import Texto from "@/app/components/Texto";
 import stylesTitulo from "../../../styles/Texto.module.css";
+import CaixaDeTexto from '@/app/components/CaixaDeTexto';
+import stylesText from '../../../styles/CaixaDeTexto.module.css'
 
 export default async function TesteBack() {
   const usuarios = await api_client.get('http://127.0.0.1:8000/usuarios/');
   console.log(usuarios)
+  const totalUsuarios = 8;
+  // Criando um array de 8 espaços vazios
+  const jogadoresPreenchidos = new Array(totalUsuarios).fill('Espaço Vazio');
+  // Preenchendo o array de jogadores com os usuários existentes
+  usuarios.forEach((usuario, index) => {
+    if (index < totalUsuarios) {
+      jogadoresPreenchidos[index] = usuario.apelido || 'Espaço Vazio';
+    }
+  });
   return (
-    <>
+    /*<>
       <Texto className={stylesTitulo.titulo} text="TESTE BACK-END" />
-      <p>{usuarios.map((usuario) => 
-      <li key={usuario.id}>
-        {usuario.apelido}
-      </li>
-        
-        
-        )}</p>
+      {usuarios.map((usuario) => 
+
       
-    </>
+      <CaixaDeTexto className={stylesCaixa.caixaDeTextoDireita} 
+      key={usuario.id}>
+        {usuario.apelido}
+      </CaixaDeTexto>
+        
+        
+        )}
+      
+    </>*/
+    <>
+    <Texto className={stylesTitulo.titulo} text="TESTE BACK-END" />
+    {jogadoresPreenchidos.map((apelido, index) => {
+      const corFundo = apelido === 'Espaço Vazio' ? '#white' : '#39BA67';
+      return (
+        <CaixaDeTexto
+          className={stylesText.caixaDeTextoDireita}
+          style={{ backgroundColor: corFundo }}
+          key={index}
+          text={apelido}
+        />
+      );
+    })}
+  </>
   );
 }
